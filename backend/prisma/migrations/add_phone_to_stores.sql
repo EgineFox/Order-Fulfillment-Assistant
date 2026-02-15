191f5db425540ba83b937208cbd6d93e3802c802
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2026-02-15 22:23:15 +0200

    Add phone migration for stores

---
+++ backend/prisma/migrations/add_phone_to_stores.sql
@@ -0,0 +1,18 @@
+UPDATE stores SET manager_phone = '+972543030519' WHERE id = 5;  -- Givatayim
+UPDATE stores SET manager_phone = '+972543030677' WHERE id = 6;  -- Natanya
+UPDATE stores SET manager_phone = '+972543030521' WHERE id = 7;  -- Raanana
+UPDATE stores SET manager_phone = '+972543030143' WHERE id = 10; -- Mamila
+UPDATE stores SET manager_phone = '+972543030263' WHERE id = 14; -- Haifa
+UPDATE stores SET manager_phone = '+972543031304' WHERE id = 22; -- 
+UPDATE stores SET manager_phone = '+972543030223' WHERE id = 24; -- Ayalon
+UPDATE stores SET manager_phone = '+972543030643' WHERE id = 26; -- Ramat Aviv
+UPDATE stores SET manager_phone = '+972547727215' WHERE id = 27; -- 
+UPDATE stores SET manager_phone = '+972543030263' WHERE id = 72; --
+UPDATE stores SET manager_phone = '+972543030516' WHERE id = 2;
+UPDATE stores SET manager_phone = '+972543030539' WHERE id = 11;
+UPDATE stores SET manager_phone = '+972543030596' WHERE id = 12;
+UPDATE stores SET manager_phone = '+972543030153' WHERE id = 16;
+UPDATE stores SET manager_phone = '+972543030529' WHERE id = 23;
+UPDATE stores SET manager_phone = '+972543030589' WHERE id = 20;
+UPDATE stores SET manager_phone = '+972543030852' WHERE id = 25;
+
