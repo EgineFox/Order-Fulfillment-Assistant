0213401b053158ec46168e7824ccb5195d226bfc
Ekaterina Morzhakova <ninorichi.israel@gmail.com>
2026-03-10 16:14:49 +0200

    Delete real phones for stores, replacing them fakes

---
+++ backend/prisma/migrations/add_phone_to_stores copy.sql
@@ -0,0 +1,18 @@
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 5;  -- Givatayim
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 6;  -- Natanya
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 7;  -- Raanana
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 10; -- Mamila
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 14; -- Haifa
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 22; -- 
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 24; -- Ayalon
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 26; -- Ramat Aviv
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 27; -- 
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 72; --
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 2;
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 11;
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 12;
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 16;
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 23;
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 20;
+UPDATE stores SET manager_phone = '+972000000000' WHERE id = 25;
+
